9197dc82f3acd0877ca37577c835692c14cd1072
ando <lifeandcoding@gmail.com>
2020-04-09 02:19:28 +0300

    update code to fix errors

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-module.exports = {
-  presets: ['@nuxt/babel-preset-app'],
-  plugins: ['@babel/plugin-proposal-optional-chaining'],
-  env: {
-    test: {
-      presets: [
-        [
+import { declare } from '@babel/helper-plugin-utils';
+
+export default declare((api) => {
+  // see docs about api at https://babeljs.io/docs/en/config-files#apicache
+  api.assertVersion('^7.0.0');
+
+  const env = process.env.BABEL_ENV || process.env.NODE_ENV;
+
+  const presets = [
+    env === 'test'
+      ? [
           '@babel/preset-env',
           {
             targets: {
               node: 'current',
             },
           },
-        ],
-      ],
-    },
-  },
-};
+        ]
+      : require.resolve('@nuxt/babel-preset-app'),
+  ];
+
+  const plugins = ['@babel/plugin-proposal-optional-chaining'];
+
+  return {
+    presets,
+    plugins,
+  };
+});
