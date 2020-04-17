599d9139732b0a400a55688ceeab107268343ce3
ando <lifeandcoding@gmail.com>
2020-04-17 23:54:12 +0300

    feat: add `@babel/plugin-syntax-dynamic-import`

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ module.exports = function babelPresetNuxt(api) {
   const plugins = [
     require('@babel/plugin-proposal-optional-chaining'),
     require('@babel/plugin-proposal-nullish-coalescing-operator'),
+    require('@babel/plugin-syntax-dynamic-import'),
   ];
 
   return {
